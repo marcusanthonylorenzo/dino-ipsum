5a3222d4c23574bcdce335e1a99a1ac3acc036c3
Marcus <marcanthonyzpadilla@gmail.com>
2022-04-19 16:30:33 -0700

    use Marvel API to search superhero database
    Co-authored-by: Brady Diamond <BradyDiamond@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import $ from "jquery";
 import "./css/styles.css";
-import Marvel from "./../src/js/objects.js";
+import { Marvel } from "./../src/js/objects.js";
 
 $(document).ready(function() {
+
+  // let audio_iframe = document.querySelector('iframe');
+  // let widget = Soundcloud.Widget(audio_iframe);
+  // widget.setVolume(0.3);
 
   $("#button").click(()=> {
     console.log("clicked");
     let out = $(".output");
+    let input = $("#input").val();
 
-    Marvel.getChar()
+    Marvel.getChar(input)
       .then(function(response){
         console.log(response);
         let array = response.data.results;
@@ -17,27 +22,18 @@ $(document).ready(function() {
         array.forEach(function(e){
           let name = e.name;
           console.log(name);
-          out.append(name);
+          let div = document.createElement("div");
+          let p = document.createElement("p");
+          let h4 = document.createElement("h4");
+          out.append(div);
+          div.append(p);
+          div.append(h4);
+          h4.textContent = name;
+          p.textContent = e.description;
           out.append(`<img src="${e.thumbnail.path}.jpg" alt="${name}">` + "<br>");
           $("img").css("animation", "slidein 500ms;");
-        });
-        out.append(response.data.results + "<br>");
-         
+        });//          
       });
 
-    Marvel.getChar2()
-      .then(function(response){
-        console.log(response);
-        let array = response.data.results;
-        console.log(response.data.results);
-
-        array.forEach(function(e){
-          let name = e.name;
-          console.log(name);
-          out.append(name);
-          out.append(`<img src="${e.thumbnail.path}.jpg" alt="${name}">` + "<br>");
-        });
-        out.append(response.data.results + "<br>");
-      });
   });
 });
--- src/js/objects.js
+++ src/js/objects.js
@@ -1,6 +1,6 @@
-export default class Marvel {  
-  static getChar() {
-    return fetch(`http://gateway.marvel.com/v1/public/characters?ts=1000&limit=100&apikey=cade37070cc22bfb00be1d9bb94a7285&hash=949d80b9f2d7c30efb76fc6cd033ee6c`)
+export class Marvel {  
+  static getChar(character) {
+    return fetch(`http://gateway.marvel.com/v1/public/characters?name=${character}&ts=1000&limit=100&apikey=cade37070cc22bfb00be1d9bb94a7285&hash=949d80b9f2d7c30efb76fc6cd033ee6c`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
@@ -11,9 +11,11 @@ export default class Marvel {
         return error;
       });
   }
+}
 
-  static getChar2() {
-    return fetch(`http://gateway.marvel.com/v1/public/characters?ts=1000&limit=100&offset=100&apikey=cade37070cc22bfb00be1d9bb94a7285&hash=949d80b9f2d7c30efb76fc6cd033ee6c`)
+export class Soundcloud {  
+  static play() {
+    return fetch("https://w.soundcloud.com/player/api.js")
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
